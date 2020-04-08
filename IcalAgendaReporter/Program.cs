9f2d9e3863982c8ab21cd7fcf08a64d7e7fb85dd
Jan Verhoeven <jmm.verhoeven@gmail.com>
2020-04-08 19:44:32 +0200

    Add project files.

---
+++ IcalAgendaReporter/Program.cs
@@ -0,0 +1,20 @@
+﻿using Ical.Net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcalAgendaReporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var icalText = File.ReadAllText(@"d:\_Jan\Antroposofie\Website\vandamhuis.nl\events.ics");
+            var calendar = Calendar.Load(icalText);
+            var events = calendar.Events.Count;
+        }
+    }
+}
