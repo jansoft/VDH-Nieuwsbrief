ea1f4239fd4f740637d0fccbe8256baf62396b85
Jan Verhoeven <jmm.verhoeven@gmail.com>
2020-08-20 10:20:53 +0200

    UI fix

--- VanDamHuisNieuwsbrief/Form1.Designer.cs
+++ VanDamHuisNieuwsbrief/Form1.Designer.cs
@@ -222,7 +222,7 @@
             this.groupBox2.Controls.Add(this.cbNewsPubdate);
             this.groupBox2.Location = new System.Drawing.Point(30, 139);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(210, 172);
+            this.groupBox2.Size = new System.Drawing.Size(210, 181);
             this.groupBox2.TabIndex = 40;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Inhoud";
